11483c7e0e1a954954863c46b06d17c4a8fda2ad
Hank <hz4514261@gmail.com>
2023-08-02 23:42:56 +0800

    fix

--- main.py
+++ main.py
@@ -7,27 +7,10 @@ class AnalyseRTCP:
         self.Data = {}
         self.PacketNumber = 1
 
-    def getPacket(self,pcapfilepath:str) -> dict:
+    def getPacket(self,pcapfilepath:str) -> None:
         PcapData = rdpcap(pcapfilepath)
         for i in PcapData:
-            IPaddr = i['IP'].src
-            PacketRaw =bytes(i['Raw'])
-            ReportCount = PacketRaw[0]
-            Type = PacketRaw[1]
-
-            if Type == 200:
-                self.GetData_From_Goodbye(PacketRaw,IPaddr)
-
-            elif Type == 201 and ReportCount == 129 :
-                self.GetRSrcID_From_RR(PacketRaw)
-            self.PacketNumber +=1
-
-        with open('DeviceID.txt','w') as f:
-            json.dump(self.IDmatch,f,indent=4)
-        
-        with open('SessionData.txt','w') as f:
-            json.dump(self.Data,f,indent=4)
-        return self.Data
+            self.CheckPacket(Packet=i)
     
     def GetRSrcID_From_RR(self,Packet) -> None:
         SrcID = int(Packet[8:12].replace(b'/',b'').hex(),16)
@@ -63,36 +46,38 @@ class AnalyseRTCP:
 
 
 if __name__ == '__main__':
-    # task = AnalyseRTCP()
-    # task.SniiffRTCP()
-    # results = task.getPacket('RTCP_From_MG.pcap')
+    task = AnalyseRTCP()
+    # task.SniiffRTCP() #透過聽封包產生資料
+    # results = task.getPacket('RTCP_From_MG.pcap') #透過 RTCP 封包檔產生資料
 
-    with open('SessionData.txt','r') as f:
-        results = json.load(f)
 
-    x_7,y_7,x_8,y_8 = [],[],[],[]
-    fig, (ax7, ax8) = plt.subplots(2, 1, sharex=True)
+    if False: #圖表開關
+        with open('SessionData.txt','r') as f:
+            results = json.load(f)
 
-    if '192.168.69.7' in results:
-        number = 1
-        for i in results['192.168.69.7']:
-            x_7.append(number)
-            y_7.append(i['Jitter'])
-            number += 1
-        ax7.plot(x_7, y_7, label='192.168.69.7')
-        ax7.set_ylim(0)
-        ax7.set_ylabel('Jitter')
-        ax7.legend()
+        x_7,y_7,x_8,y_8 = [],[],[],[]
+        fig, (ax7, ax8) = plt.subplots(2, 1, sharex=True)
 
-    if '192.168.69.8' in results:
-        number = 1
-        for i in results['192.168.69.8']:
-            x_8.append(number)
-            y_8.append(i['Jitter'])
-            number += 1
-        ax8.plot(x_8, y_8, label='192.168.69.8')
-        ax8.set_ylim(0)
-        ax8.set_ylabel('Jitter')
-        ax8.legend()
+        if '192.168.69.7' in results:
+            number = 1
+            for i in results['192.168.69.7']:
+                x_7.append(number)
+                y_7.append(i['Jitter'])
+                number += 1
+            ax7.plot(x_7, y_7, label='192.168.69.7')
+            ax7.set_ylim(0)
+            ax7.set_ylabel('Jitter')
+            ax7.legend()
 
-    plt.show()
+        if '192.168.69.8' in results:
+            number = 1
+            for i in results['192.168.69.8']:
+                x_8.append(number)
+                y_8.append(i['Jitter'])
+                number += 1
+            ax8.plot(x_8, y_8, label='192.168.69.8')
+            ax8.set_ylim(0)
+            ax8.set_ylabel('Jitter')
+            ax8.legend()
+
+        plt.show()
