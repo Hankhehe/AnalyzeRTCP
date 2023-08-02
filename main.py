84f653711193ab7ac797c531eadbd1d7a7223fa7
Hank <hz4514261@gmail.com>
2023-08-03 00:53:18 +0800

    增加 delay time 分析

--- main.py
+++ main.py
@@ -6,33 +6,32 @@ class AnalyseRTCP:
         self.IDmatch = {}
         self.Data = {}
         self.PacketNumber = 1
-
-    def getPacket(self,pcapfilepath:str) -> None:
-        PcapData = rdpcap(pcapfilepath)
-        for i in PcapData:
-            self.CheckPacket(Packet=i)
     
-    def GetRSrcID_From_RR(self,Packet) -> None:
+    def Get_SrcID_From_RR(self,Packet) -> None:
+        '''傳進 RR 封包並解析出 SrcID、Lost、DelayTime'''
         SrcID = int(Packet[8:12].replace(b'/',b'').hex(),16)
         PacketsLost = int(Packet[13:16].replace(b'/',b'').hex(),16)
         Jitter = int(Packet[20:24].replace(b'/',b'').hex(),16)
+        DelayTime = int(Packet[28:32].replace(b'/',b'').hex(),16)
         if not self.IDmatch[SrcID] in self.Data : self.Data[self.IDmatch[SrcID]] = []
-        self.Data[self.IDmatch[SrcID]].append({'ID':SrcID,'Jitter':Jitter,'PacketLost':PacketsLost})
+        self.Data[self.IDmatch[SrcID]].append({'ID':SrcID,'Jitter':Jitter,'PacketLost':PacketsLost,'DelayTime':DelayTime})
 
-    def GetData_From_Goodbye(self,Packet,IPAddr) -> None:
+    def Get_Data_From_SR(self,Packet,IPAddr) -> None:
+        '''傳進 SR 封包，將 Src ID 存進 Data'''
         SrcID =int(Packet[4:8].replace(b'/',b'').hex(),16)
         self.IDmatch[SrcID] = IPAddr
 
     def CheckPacket(self,Packet) -> None:
+        '''解析 RTCP 封包，並將分類成 RR or SR'''
         IPaddr = Packet['IP'].src
         PacketRaw =bytes(Packet['Raw'])
         ReportCount = PacketRaw[0]
         Type = PacketRaw[1]
-        if Type == 200:
-            self.GetData_From_Goodbye(PacketRaw,IPaddr)
+        if Type == 200: 
+            self.Get_Data_From_SR(PacketRaw,IPaddr)
 
         elif Type == 201 and ReportCount == 129 :
-            self.GetRSrcID_From_RR(PacketRaw)
+            self.Get_SrcID_From_RR(PacketRaw)
         self.PacketNumber +=1
 
         with open('DeviceID.txt','w') as f:
@@ -41,14 +40,21 @@ class AnalyseRTCP:
         with open('SessionData.txt','w') as f:
             json.dump(self.Data,f,indent=4)
 
+    def Get_Packet(self,pcapfilepath:str) -> None:
+        '''透過 pcap 封包檔解析 RTCP'''
+        PcapData = rdpcap(pcapfilepath)
+        for i in PcapData:
+            self.CheckPacket(Packet=i)
+
     def SniiffRTCP(self) -> None:
+        '''透過聽封包進行 RTCP 分析'''
         sniff(filter = 'udp and port 5005',store = 0,prn=self.CheckPacket,iface='乙太網路 4')
 
 
 if __name__ == '__main__':
     task = AnalyseRTCP()
     # task.SniiffRTCP() #透過聽封包產生資料
-    # results = task.getPacket('RTCP_From_MG.pcap') #透過 RTCP 封包檔產生資料
+    # results = task.Get_Packet('RTCP_From_MG.pcap') #透過 RTCP 封包檔產生資料
 
 
     if False: #圖表開關
