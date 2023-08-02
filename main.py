f77f1811bb75751c572e8a1b6ce496099448cc2d
HankChen <hank@pixis.com.tw>
2023-08-02 17:57:38 +0800

    fix

--- main.py
+++ main.py
@@ -40,17 +40,32 @@ class AnalyseRTCP:
         SrcID =int(Packet[4:8].replace(b'/',b'').hex(),16)
         self.IDmatch[SrcID] = IPAddr
 
+    def CheckPacket(self,Packet) -> None:
+        IPaddr = Packet['IP'].src
+        PacketRaw =bytes(i['Raw'])
+        ReportCount = PacketRaw[0]
+        Type = PacketRaw[1]
+        if Type == 200:
+            self.GetData_From_Goodbye(PacketRaw,IPaddr)
+
+        elif Type == 201 and ReportCount == 129 :
+            self.GetRSrcID_From_RR(PacketRaw)
+        self.PacketNumber +=1
+
+        with open('DeviceID.txt','w') as f:
+            json.dump(self.IDmatch,f,indent=4)
+        
+        with open('SessionData.txt','w') as f:
+            json.dump(self.Data,f,indent=4)
+
     def SniiffRTCP(self) -> None:
-        pass
-        # sniff(iface='乙太網路'filter = 'udp and port 5055',)
+        sniff(filter = 'udp and port 5055',store = 0,prn=self.CheckPacket,iface='乙太網路')
 
 
 if __name__ == '__main__':
     task = AnalyseRTCP()
-    # PcapData = rdpcap('4.pcap')
-    # sniff(iface='乙太網路'filter = 'udp and port 5055')
-    
-    results = task.getPacket('4.pcap')
+    # task.SniiffRTCP()
+    results = task.getPacket('RTCP_From_MG.pcap')
     
     x_7,y_7,x_8,y_8 = [],[],[],[]
     fig, (ax7, ax8) = plt.subplots(2, 1, sharex=True)
