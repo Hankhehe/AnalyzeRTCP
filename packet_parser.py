1801bff5236948f468ff9181b5087004b36a7588
MakotoAtsu <7738420+MakotoAtsu@users.noreply.github.com>
2023-08-04 18:19:26 +0000

    Refactor: parse_rtcp_payload will raise ex if parse fial

--- packet_parser.py
+++ packet_parser.py
@@ -3,8 +3,7 @@ from scapy.packet import Packet
 from kaitaistruct import KaitaiStream, BytesIO
 from models.rtcp_payload import RtcpPayload
 
-def parse_rtcp_packet(packet:Packet) -> list[RtcpPayload.RtcpPacket] | None:
-    try:
+def parse_rtcp_payload(packet:Packet) -> RtcpPayload:
         if not packet.haslayer('UDP'):
             return []
         
@@ -12,32 +11,34 @@ def parse_rtcp_packet(packet:Packet) -> list[RtcpPayload.RtcpPacket] | None:
         if packet.haslayer('Padding'):
             del packet['Padding']
 
-        datas = RtcpPayload(KaitaiStream(BytesIO(bytes(packet['UDP'].payload))))
-        return datas.rtcp_packets
-    except Exception as e:
-        print("Cannot parse rtcp payload, maybe it's not a RTCP packets")
-        print('udp payload: ')
-        hexdump(packet['UDP'].payload)
-        print("Exception: ", e)
-        return None
-
+        payload = RtcpPayload(KaitaiStream(BytesIO(bytes(packet['UDP'].payload))))
+        return payload
+    
 
 if __name__ == '__main__':
     # Replace 'your_pcap_file.pcap' with the actual path to your PCAP file
-    pcap_file = 'RTCP_From_MG.pcap'
+    # pcap_file = 'RTCP_From_MG.pcap'
+    pcap_file = '4.pcap'
 
-    rtcps = []
+    rtcp_payloads: list[RtcpPayload] = []
     err_cnt = 0
     # Iterate through the packets and parse RTCP headers
     for idx, packet in enumerate(rdpcap(pcap_file)):
-        datas = parse_rtcp_packet(packet)
-        if not datas:
-            print('current packet id: ', idx + 1)
-            print("=====================================")
+        try:
+            datas = parse_rtcp_payload(packet)
+            rtcp_payloads.append(datas)
+        except Exception as e:
             err_cnt += 1
-            continue
-        
-        rtcps.extend(datas)
-    
-    print('Total err packets: ', err_cnt)
-    
+            print("=====================================")
+            print('current packet id: ', idx + 1)
+            print("Cannot parse rtcp payload, maybe it's not a RTCP packets")
+            print('udp payload: ')
+            hexdump(packet['UDP'].payload)
+            print("Exception: ", e)
+
+    print('Total packets: ', len(rtcp_payloads) + err_cnt)
+    print('RTCP payloads: ', len(rtcp_payloads))
+    print('Error packets: ', err_cnt)
+    rtcp_packets: list[RtcpPayload.RtcpPacket] = [pack for payload in rtcp_payloads for pack in payload.rtcp_packets]
+    print('Total RTCP Packets: ', len(rtcp_packets))
+    print('Goodbye packet count: ', len([p for p in rtcp_packets if p.payload_type == RtcpPayload.PayloadType.bye]))
